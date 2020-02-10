bfddc28281400f67a44de6cadb2aad63bd3fbba8
Neta Nir <neta@amazon.com>
2020-02-09 18:55:48 -0800

    include lambda file

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -7,10 +7,6 @@ export class MyPipeline extends cdk.Construct {
     constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
         super(scope, id);
         const pipeline = new codepipeline.Pipeline(this, 'MyPipeline');
-        const dAction = new codepipeline_actions.CloudFormationCreateReplaceChangeSetAction({
-            actionName: 'deploy',
-
-        })
         pipeline.addStage({
             stageName: 'beta',
             actions: [
