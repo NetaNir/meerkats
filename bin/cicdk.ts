bdd4e2169cafc5ed2cf37e1f53ac992684775d9a
Adam Ruka <adamruka@amazon.com>
2020-02-11 15:22:38 -0800

    Remove the VPC stack, as it's no longer used.
    Also make the App env-agnostic to make it easier to deploy it locally.

--- bin/cicdk.ts
+++ bin/cicdk.ts
@@ -3,25 +3,10 @@ import 'source-map-support/register';
 import * as cdk from '@aws-cdk/core';
 import { APIGWStack } from '../lib/agigw-stack';
 import { DDBStack } from '../lib/ddb-stack';
-import { VPCStack } from '../lib/vpc';
 
 const app = new cdk.App();
-const prod = {
-    region: 'us-west-2',
-    account: process.env.BETA_ACCOUNT
-}
 
-const vpcStack = new VPCStack(app, "VPCStack", {
-    env: prod
-});
-const apigw = new APIGWStack(app, 'APIGWStack', {
-    env: prod, 
-    vpc: vpcStack.vpc
-});
+const apigw = new APIGWStack(app, 'APIGWStack');
 new DDBStack(app, 'DDBStack', {
-    env: prod,
-    grantRead: [ apigw.handler ]
+  grantRead: [apigw.handler]
 });
-
-
-
