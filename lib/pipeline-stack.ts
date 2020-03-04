307937699301b9da9f98ac4e4b66c3a47a90bb7c
Rico Huijbers <huijbers@amazon.com>
2020-03-04 11:53:51 +0100

    Fix indentation

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -38,7 +38,7 @@ export class PipelineStack extends cdk.Stack {
         }
       })
     });
-    
+
     const euEnv: Environment = {
       account: '355421412380',
       region: 'eu-west-1'
@@ -52,65 +52,64 @@ export class PipelineStack extends cdk.Stack {
     pipeline.addCdkStage(beta);
     const gamma = createStage(scope, euEnv, 'gamma', true);
     pipeline.addCdkStage(gamma)
-    };
-  }
-  
-  function createStage(scope: Construct, env: Environment, name: string, addValidation: boolean): CdkStage {
-    const ddbStack = new DDBStack(scope, `Meerkats-DDBStack-${name}`, {
-      env
-    });
-    const apiGwStack = new APIGWStack(scope, `Meerkats-APIGWStack-${name}`, {
-      env,
-      table: ddbStack.table
-    });
-    // Change this line to 'true' to see synthesis fail with a validation error
-    const breakValidation = false;
-    const twiddleStacks = breakValidation ? reversed : identity;
-    // this is the artifact that will record the output containing the generated URL of the API Gateway
-    // Need to explicitly name it because the stage name contains a '.' and that's not allowed to be in the artifact name.
-    const apiGwStackOutputs = new codepipeline.Artifact(`Artifact_${name}_APIGWStack_Output`);
-    const stage = new CdkStage({
-      stageName: name,
-      stacks: twiddleStacks([{
-        stack: ddbStack
-      }, {
-        stack: apiGwStack,
-        outputsArtifact: apiGwStackOutputs
-      }])
-    });
-    if (addValidation) {
-      stage.addValidations(
-        new ShellCommandsValidation({
-          name: `IntegTest-${name}`,
-          input: apiGwStackOutputs,
-          commands: [
-          'set -e',
-          // take out the URL of the API Gateway from the outputs.json file produced by the previous CFN deploy Action
-          `api_gw_url=$(node -e 'console.log(require("./outputs.json")["${APIGWStack.URL_OUTPUT}"]);')`,
-          'curl $api_gw_url',    
-          ]
-        })
-      );
-    }
-    return stage;
-  }
+  };
+}
 
-  function copyEnvironmentVariables(...names: string[]): Record<string, codebuild.BuildEnvironmentVariable> {
-    const ret: Record<string, codebuild.BuildEnvironmentVariable> = {};
-    for (const name of names) {
-      if (process.env[name]) {
-        ret[name] = { value: process.env[name] };
-      }
+function createStage(scope: Construct, env: Environment, name: string, addValidation: boolean): CdkStage {
+  const ddbStack = new DDBStack(scope, `Meerkats-DDBStack-${name}`, {
+    env
+  });
+  const apiGwStack = new APIGWStack(scope, `Meerkats-APIGWStack-${name}`, {
+    env,
+    table: ddbStack.table
+  });
+  // Change this line to 'true' to see synthesis fail with a validation error
+  const breakValidation = false;
+  const twiddleStacks = breakValidation ? reversed : identity;
+  // this is the artifact that will record the output containing the generated URL of the API Gateway
+  // Need to explicitly name it because the stage name contains a '.' and that's not allowed to be in the artifact name.
+  const apiGwStackOutputs = new codepipeline.Artifact(`Artifact_${name}_APIGWStack_Output`);
+  const stage = new CdkStage({
+    stageName: name,
+    stacks: twiddleStacks([{
+      stack: ddbStack
+    }, {
+      stack: apiGwStack,
+      outputsArtifact: apiGwStackOutputs
+    }])
+  });
+  if (addValidation) {
+    stage.addValidations(
+      new ShellCommandsValidation({
+        name: `IntegTest-${name}`,
+        input: apiGwStackOutputs,
+        commands: [
+        'set -e',
+        // take out the URL of the API Gateway from the outputs.json file produced by the previous CFN deploy Action
+        `api_gw_url=$(node -e 'console.log(require("./outputs.json")["${APIGWStack.URL_OUTPUT}"]);')`,
+        'curl $api_gw_url',
+        ]
+      })
+    );
+  }
+  return stage;
+}
+
+function copyEnvironmentVariables(...names: string[]): Record<string, codebuild.BuildEnvironmentVariable> {
+  const ret: Record<string, codebuild.BuildEnvironmentVariable> = {};
+  for (const name of names) {
+    if (process.env[name]) {
+      ret[name] = { value: process.env[name] };
     }
-    return ret;
   }
-  
-  function identity<A>(x: A): A {
-    return x;
-  }
-  
-  function reversed<A>(xs: A[]): A[] {
-    xs.reverse();
-    return xs;
-  }
-  
+  return ret;
+}
+
+function identity<A>(x: A): A {
+  return x;
+}
+
+function reversed<A>(xs: A[]): A[] {
+  xs.reverse();
+  return xs;
+}
