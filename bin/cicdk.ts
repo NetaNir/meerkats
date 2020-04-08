58ab05ecea869d73216a75176680cf27bfbfe3dc
Rico Huijbers <rix0rrr@gmail.com>
2020-04-08 14:45:14 +0200

    Add pipeline

--- bin/cicdk.ts
+++ bin/cicdk.ts
@@ -10,4 +10,12 @@ new MyApplication(app, 'DevApp', {
   env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
 });
 
+const pipeline = new MyPipelineStack(app, 'PipelineStack', {
+  env: { account: '355421412380', region: 'eu-west-1' },
+});
+
+pipeline.addStage(new MyApplication(app, 'Beta', {
+  env: { account: '355421412380', region: 'eu-west-1' },
+}));
+
 app.synth();
